8f0691f92d2442f305710a9fd1472fdcc6567969
Lew1nd <eltomybarrios@gmail.com>
2024-11-18 14:08:37 -0300

    Feat: Se eliminaron redundancias en algunas partes del código. Se creó una función para mostrar texto e imagenes en la pantalla, y una función para iniciar la música

--- MenuConfig.py
+++ MenuConfig.py
@@ -1,32 +1,45 @@
 '''
-Aquí estará todo lo relacionado a la pantalla de menú
+Aquí estará todo lo relacionado a las pantallas
 '''
 import pygame
 import StaticFunctions
 from Colors import *
-#region Menu
+#region Menú
 class Menu():
     def __init__(self):
+        self.menu = StaticFunctions.iniciar_pantalla()
+        pygame.display.set_caption ("BGB")
+        icono = pygame.image.load("archivos_multimedia/imagenes/icono_preguntados.png")#Carga el incono de la ventana
+        StaticFunctions.iniciar_musica(0.6,-1,"archivos_multimedia/musica/musica_menu.mp3")
+        pygame.display.set_icon(icono)
         self.screen_color = BLUEVIOLET
 
+        #botones
         self.button_start = [500, 250,250,100]#Posición x, posición Y, Largo, alto
         self.button_score = [500, 450,250,100]
         self.button_exit = [500,650,250,100]
+        self.button_music = [1180, 20, 100, 70]
 
-        self.menu_buttons = {'Start': self.button_start,
-                             'Score': self.button_score,
-                             'Exit': self.button_exit}
-    
+        #textos
+        self.text_tittle = [470,100]
+
+        #imagen muestra (temporal)
+        self.imagen_1 = [0,0,500,500]
+
+        self.muteado = False
+
     def init_menu(self):
-        start_button = StaticFunctions.generar_rectangulo(self.menu_buttons['Start'])
-        score_button = StaticFunctions.generar_rectangulo(self.menu_buttons['Score'])
-        exit_button = StaticFunctions.generar_rectangulo(self.menu_buttons['Exit'])
+        tittle_text = StaticFunctions.dibujar_texto(self.menu, "Preguntados", 70, GREEN, self.text_tittle)
+        start_button = StaticFunctions.generar_rectangulo(self.menu, GREEN, self.button_start)
+        score_button = StaticFunctions.generar_rectangulo(self.menu, YELLOW1, self.button_score)
+        music_button = StaticFunctions.generar_rectangulo(self.menu, BLUE, self.button_music)
+        exit_button = StaticFunctions.generar_rectangulo(self.menu, RED1, self.button_exit)
 
+        #img1 = StaticFunctions.dibujar_imagen(self.menu, "archivos_multimedia/imagenes/icono_preguntados.png", self.imagen_1)#imagen de preguntados
+        
         if StaticFunctions.change_screen_flag:
-            self.menu = StaticFunctions.iniciar_pantalla()
             self.menu.fill(self.screen_color)
             StaticFunctions.change_screen_flag = False
-
 
         event_game = pygame.event.get()
         for event in event_game:#Eventos
@@ -39,33 +52,36 @@ class Menu():
                 elif score_button.collidepoint(pygame.mouse.get_pos()):
                     StaticFunctions.current_screen = StaticFunctions.cambiar_pantalla("ScoreBoard")
                     StaticFunctions.change_screen_flag = True
-
+                
+                elif music_button.collidepoint(pygame.mouse.get_pos()):
+                    self.muteado = not self.muteado
+                    if self.muteado:
+                        pygame.mixer.music.set_volume(0)#Silencia la música
+                    else:
+                        pygame.mixer.music.set_volume(0.6)#Vuelve a sonar la musica
                 elif exit_button.collidepoint(pygame.mouse.get_pos()):
                     print("Juego cerrado")
                     return False
-
-        pygame.draw.rect(self.menu, GREEN, start_button)
-        pygame.draw.rect(self.menu, YELLOW1, score_button)
-        pygame.draw.rect(self.menu, RED1, exit_button)
         return True
 #endregion
 #region Score
 class Scoreboard():
     def __init__(self):
+        self.score = StaticFunctions.iniciar_pantalla()
         self.screen_color = GREEN1
 
         self.button_back = [500,650,250,100]
+        self.button_music = [1180, 20, 100, 70]
 
-        self.score_buttons = {'Back': self.button_back}
-
-        
+        self.muteado = False
 
     def init_scoreboard(self):
-        back_button = StaticFunctions.generar_rectangulo(self.score_buttons['Back'])
+        back_button = StaticFunctions.generar_rectangulo(self.score, RED1, self.button_back)
+        music_button = StaticFunctions.generar_rectangulo(self.score, BLUE, self.button_music)
 
         if StaticFunctions.change_screen_flag:
-            self.score = StaticFunctions.iniciar_pantalla()
             self.score.fill(self.screen_color)
+            StaticFunctions.iniciar_musica(0.2,-1,"archivos_multimedia/musica/musica_score.mp3")
             StaticFunctions.change_screen_flag = False
         
         event_game = pygame.event.get()
@@ -74,12 +90,15 @@ class Scoreboard():
                 return False
 
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:#Evento generado al presionar el click izquierdo
+                if music_button.collidepoint(pygame.mouse.get_pos()):
+                    self.muteado = not self.muteado
+                    if self.muteado:
+                        pygame.mixer.music.set_volume(0)#Silencia la música
+                    else:
+                        pygame.mixer.music.set_volume(0.2)#Vuelve a sonar la musica
                 if back_button.collidepoint(pygame.mouse.get_pos()):
                     StaticFunctions.current_screen = StaticFunctions.cambiar_pantalla("Menu")
+                    StaticFunctions.iniciar_musica(0.6,-1,"archivos_multimedia/musica/musica_menu.mp3") 
                     StaticFunctions.change_screen_flag = True
-
-
-
-        pygame.draw.rect(self.score, RED1, back_button)
         return True
 #endregion
--- StaticFunctions.py
+++ StaticFunctions.py
@@ -14,15 +14,49 @@ def iniciar_pantalla():
     pantalla = pygame.display.set_mode((1280,800))
     return pantalla
 
-def generar_rectangulo(button_dimensions: list):
+def generar_rectangulo(surface, color, button_dimensions: list):
     '''
     Genera un objeto de tipo rectángulo.\n
-    Recibe una lista con las dimensiones del rectángulo, además de su posición.\n
+    Recibe una lista con las dimensiones del rectángulo y su posición, además del color y la superficie (pantalla actual).\n
     Devuelve el objeto.
     '''
-    boton = pygame.Rect(button_dimensions)
+    rectangulo = pygame.Rect(button_dimensions)
+    boton = pygame.draw.rect(surface, color, rectangulo)
     return boton
+
+def dibujar_texto(surface , string: str, font_size: int, color, position: list, font = "Arial"):
+    '''
+    Genera un texto y lo muestra en pantalla.\n
+    Recibe la pantalla, la cadena, el tamaño de la fuente, un color, la posición en pantalla y una fuente (por defecto es Arial).\n
+    Devuelve el objeto
+    '''
+    text_font = pygame.font.SysFont(font, font_size)#Se guarda la fuente que se utilizará en el texto
+    text_txt = text_font.render(string, True, color)#Posteriormente, se crea el texto
+    text_object = surface.blit(text_txt, position)#Y luego se muestra en la pantalla
+    return text_object
+
+def dibujar_imagen(surface, path: str, imagen_dimensions: list):
+    '''
+    Dibuja una imagen.\n
+    Recibe la pantalla, la ubicación del archivo y las dimensiones de la imagen (tamaño y ubicación en pantalla).\n
+    Devuelve el objeto 
+    '''
+    img = pygame.image.load(path)
+    img_object = pygame.transform.scale(img, (imagen_dimensions[2], imagen_dimensions[3]))
+    surface.blit(img_object, (imagen_dimensions[0], imagen_dimensions[1]))
+    return img_object
 
 def cambiar_pantalla(name: str) -> str:
     print("Pantalla cambiada a", name)
     return name
+
+def iniciar_musica(volume: float, loops: int, path: str):
+    '''
+    Inicia la música.\n
+    Recibe el volumen, la cantidad de veces que se va a repetir (loop) y la ubicación del archivo en la carpeta (path).\n
+    No devuelve nada
+    '''
+    pygame.mixer.music.stop()#Para la musica
+    pygame.mixer.music.load(path)#Carga la musica para el menu
+    pygame.mixer.music.set_volume(volume)#Establece el volumen
+    pygame.mixer.music.play(loops)#Veces que se repetirá (-1 para infinitas veces)
